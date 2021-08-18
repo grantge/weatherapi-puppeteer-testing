9749a748f7c0346bbb502070d44b67ad9b0ebcc5
grantge <grantfromgeorgia@gmail.com>
2021-08-18 23:29:30 +0300

    add test

--- testWeatherApi.js
+++ testWeatherApi.js
@@ -11,7 +11,6 @@ const selectorLabel = '.col-form-label';
 // Browser functions
 const dontRunBrowser = false;
 
-
 class TestSuit {
 
     // Check the title of Wather api
@@ -88,6 +87,37 @@ class TestSuit {
         await page.goto(link)
     
         console.log('Find label of the app')
+        let element = await page.$(selectorLabel)
+        let result = await page.evaluate(element => element.textContent, element)
+        console.log(result)
+    
+        console.log('Compare app label')
+        
+        if(result == 'City:') {
+            console.log("\x1b[36m%s\x1b[0m",'the label of the app is City:, Good')
+        }
+        else {
+            console.log("\x1b[31m%s\x1b[0m",'the label of the app is not City:, Error')
+        }
+    
+        console.log("\x1b[33m%s\x1b[0m",'Close the browser')
+        await browser.close();
+    }
+
+    // Check the placeholder of the app 
+    async testCheckLabel() {
+        console.log('\x1b[35m%s\x1b[0m', 'Test 4 is running');
+        
+        console.log("\x1b[33m%s\x1b[0m",'Browser is running')
+        const browser = await puppeeter.launch({ headless: dontRunBrowser });
+    
+        console.log('Create a new page')
+        const page = await browser.newPage();
+    
+        console.log('Go to the page https://gentle-oasis-79578.herokuapp.com/')
+        await page.goto(link)
+    
+        console.log('Find placeholder of the app')
         let element = await page.$(selectorLabel)
         let result = await page.evaluate(element => element.textContent, element)
         console.log(result)
